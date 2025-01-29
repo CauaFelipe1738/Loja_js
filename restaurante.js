3583b40c596b0319c6acc4fab4819249ff1926e1
CauaFelipe1738 <kaua.carvalho.1bmangal@gmail.com>
2025-01-29 16:59:09 +0000

    atividade feita

---
+++ restaurante.js
@@ -0,0 +1,15 @@
+let btCalcular = document.getElementById('btCalcular')
+
+btCalcular.addEventListener('click', calcularPreco)
+
+function calcularPreco(){
+    let inQuilo = document.getElementById('inQuilo')
+    let inConsumo = document.getElementById('inConsumo')
+    let outValor = document.getElementById('outValor')
+
+    let quilo = Number(inQuilo.value)
+    let consumo = Number(inConsumo.value)
+
+    let valor = (quilo/1000) * consumo
+    outValor.innerHTML = "O valor total será de R$"+ valor.toFixed(2)
+}
